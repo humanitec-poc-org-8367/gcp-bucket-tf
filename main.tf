7332ba6551c8aa3c84fe7302c59cce984df6efcc
William Overton <willovertonuk@gmail.com>
2023-06-30 13:56:28 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,77 @@
+/*
+ Terraform module to create a GCP storage bucket.
+ This module will create a GCP storage bucket and return the bucket name and region.
+ It also proxies credentials for the workload to access the created bucket.
+ 
+ Required variables:
+ - gcp_credentials_b64: base64 encoded GCP credentials
+ - gcp_project: GCP project name
+ - bucket_location: GCP bucket location
+ - app_name: Application name
+ - env_name: Environment name
+ - resource_name: Resource name
+ - workload_access_credentials_b64: base64 encoded credentials for the workload to access created buckets
+
+ Why workload_access_credentials_b64?
+  The workload needs to access the created bucket. Ideally we would create a service account for the workload and grant it access to the bucket.
+  However for this simple implementation we will just proxy the workload credentials to the bucket.
+  The credentials used will be created externally and scoped to only read/write buckets in this gcp project.
+*/
+
+variable gcp_credentials_b64 {
+  type = string
+  sensitive = true
+}
+
+variable gcp_project {
+  type = string
+}
+
+variable bucket_location {
+  type = string
+  default = "US"
+}
+
+variable app_name {
+  type = string
+}
+
+variable env_name {
+  type = string
+}
+
+variable resource_name {
+  type = string
+}
+
+variable workload_access_credentials_b64 {
+  type = string
+  sensitive = true
+  description = "Credentials for the workload to access created buckets"
+}
+
+provider "google" {
+  credentials = base64decode(var.gcp_credentials_b64)
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = replace(lower("${var.app_name}-${var.env_name}-${var.resource_name}"), " ", "_")
+  location      = var.bucket_location
+  force_destroy = true // Allow terraform to destroy the bucket.
+
+  public_access_prevention = "enforced" // Never allow public access to this bucket
+}
+
+output "bucket" {
+  value = google_storage_bucket.bucket.name
+}
+
+output "region" {
+  value = google_storage_bucket.bucket.location
+}
+
+output "credentials" {
+  value = base64decode(var.workload_access_credentials_b64)
+  description = "Credentials for the workload to access created buckets"
+  sensitive = true
+}
